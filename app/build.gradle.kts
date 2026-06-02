74e03d8a9db86a4b15454afce06774459097cd2f
KenzBilal <kenzibilal@gmail.com>
2026-06-02 15:34:27 +0530

    chore: bump version to 2.3.10 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.kaze"
         minSdk = 24
         targetSdk = 35
-        versionCode = 42
-        versionName = "2.3.9"
+        versionCode = 43
+        versionName = "2.3.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
